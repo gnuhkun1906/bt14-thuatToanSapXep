6f21b9c34a305dde2d388fc1a872dec553a55ea0
unknown <vanhungkfc190699@gmail.com>
2023-03-28 15:10:10 +0700

    bt

---
+++ src/bt2/InsertionSort.java
@@ -0,0 +1,34 @@
+package bt2;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+import java.util.Scanner;
+
+public class InsertionSort {
+    public static void main(String[] args) {
+        List<Integer> list = new ArrayList<Integer>();
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Nhập vào số phần tử muốn thêm");
+        int count = Integer.parseInt(sc.nextLine());
+        for (int i = 0; i < count; i++) {
+            list.add(new Random().nextInt(20));
+        }
+        System.out.println("Show list");
+        System.out.println(list);
+        insertSort(list);
+    }
+    public static void insertSort(List<Integer> list){
+        for (int i = 1; i < list.size() ; i++) {
+
+            for (int j = i-1; j >=0 ; j--) {
+                if (list.get(j+1)< list.get(j)){
+                    int temp = list.get(j);
+                    list.set(j, list.get(j+1));
+                    list.set((j+1), temp);
+                }
+            }
+        }
+        System.out.println(list);
+    }
+}
